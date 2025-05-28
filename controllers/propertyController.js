c01701e34a72a8d40b6d3e5746d1aa035f3e3dd3
tobak95 <akinola.tobi.2010@gmail.com>
2025-05-28 09:29:11 -0600

    avaliability added

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -68,12 +68,18 @@ const UpdatePropertyAvailability = async (req, res) => {
 // Firstly we start with This function that retrieves all properties from the database
 const getAllProperties = async (req, res) => {
   try {
-    const { page = 1, location, budget, type } = req.query; //location was not used in the original code, but you can implement filtering based on location if needed.
+    const { page = 1, location, budget, type, availability } = req.query; //location was not used in the original code, but you can implement filtering based on location if needed.
     const limit = 12;
     const skip = (page - 1) * limit;
     const filter = {
       availability: "available",
     };
+
+    if (availability) {
+      filter.availability = availability;
+    }
+  
+
     if (location) {
       filter.location = { $regex: location, $options: "i" }; // Case-insensitive search for location
     }
